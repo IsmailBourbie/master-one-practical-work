8447fbbaddb977366686cbac238030c195828031
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-17 19:13:09 +0100

    AC: Making Changment in The PCA Python File

--- algorithm_complexity/project/pca.py
+++ algorithm_complexity/project/pca.py
@@ -15,30 +15,35 @@ def pca(number_compontents, data , is_scaled = True):
     cov_matrix = np.cov(data.T)
     
     # calculate the eigen things
-    eig_vals , eig_vect = np.linalg.eig(cov_matrix)
+    eig_vals , eig_vect = np.linalg.eigh(cov_matrix)
     
+    indexes = np.argsort(-eig_vals)
+    eig_vals = eig_vals[indexes] # sort in proper order
+    eig_vals = np.maximum(eig_vals, 0) # get rid of negatives
+    eig_vecs = eig_vecs[:,indexes]
+
     # Forming a pair of Eign Values / Pairs
     """eig_pairs = []
     for i in range(len(eig_vals)):
         eig_pairs.append((np.abs(eig_vals[i]) , eig_vect[:,i])))"""
     
     # codes are the same
-    eig_pairs = [(np.abs(eig_vals[i]) , eig_vect[:,i]) for i in range(len(eig_vals))]
+    # eig_pairs = [(np.abs(eig_vals[i]) , eig_vect[:,i]) for i in range(len(eig_vals))]
 
     # Sorting All of Them
-    eig_pairs.sort(key = lambda x : x[0] , reverse= True)
+    # eig_pairs.sort(key = lambda x : x[0] , reverse= True)
     
-    final = []
-    for i in range(number_compontents):
-        final.append(eig_pairs[i][1].reshape(data.shape[1],1))
+    # final = []
+    # for i in range(number_compontents):
+    #    final.append(eig_pairs[i][1].reshape(data.shape[1],1))
 
     # Creating the Projection Matrix
-    projection_matrix = np.hstack((final))
+    # projection_matrix = np.hstack((final))
     
     # transforming the data
-    Y = data.dot(projection_matrix)
+    Y = data.dot(eig_vecs)
     
-    return Y
+    return Y[:,:number_compontents]
     
         
         
