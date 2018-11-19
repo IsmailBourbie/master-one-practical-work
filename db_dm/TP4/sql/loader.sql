7858a2e3b1fba035a18f90d5656c7ea47c397d32
Ismail Bourbie <bourbieismail@gmail.com>
2018-11-19 23:37:44 +0100

    DB_DM: start with TP4

--- db_dm/TP4/sql/loader.sql
+++ db_dm/TP4/sql/loader.sql
@@ -1,1 +1,5 @@
-LOAD DATA INFILE 'C:\\Users\\Ismail Bourbie\\Desktop\\dates.txt' INTO TABLE dates FIELDS TERMINATED BY '|';
+LOAD DATA INFILE 'C:\\Users\\Ismail Bourbie\\Desktop\\dates.txt' 
+    INTO TABLE dates FIELDS TERMINATED BY '|';
+
+
+    
