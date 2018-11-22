b8c66416cd90154a323b5ff82019900e3e57e19f
Younes Charfaoui <mxcsyounes@gmail.com>
2018-11-22 18:27:35 +0100

    AC : testing the pca algorithm with sklearn one.

--- algorithm_complexity/project/code_files/time_testing.py
+++ algorithm_complexity/project/code_files/time_testing.py
@@ -5,56 +5,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pca import pca
 import time as t
-
-def pcav(number_compontents, data , is_scaled = True):  
-
-    u,s,v = np.linalg.svd(data.T)
-
-    eig_pairs = [(np.abs(u[i]) , s[:,i]) for i in range(len(u))]    
-
-    eig_pairs.sort(key = lambda x : x[0] , reverse= True)  
-
-    final=[]
-    for i in range(number_compontents):  
-	       final.append(eig_pairs[i][1].reshape(data.shape[1],1))  
-	
-	    	# Creating the Projection Matrix  
-    projection_matrix = np.hstack((final))  
-	
-	   	 # transforming the data  
-    Y = data.dot (projection_matrix)  
-	
-    return Y  
-
-
-s = 40
-result = []
-for i in range(s):
-    data = np.random.rand(i*100,i*100)
-    start = t.time()
-    new = pca(i,data)
-    end = t.time()
-    result.append(end-start)
-    
-x = np.linspace(0,s,num=s)
-plt.plot(x , result , c = 'b')
-plt.show()
+from sklearn.decomposition import PCA
 
 def x_cube(x):
-    return x**3
+    return (x**3)/700
 
-plt.plot(x , x_cube(x) , c = 'b')
+result_our = []
+result_sklearn = []
+
+s = 40
+
+for i in range(s):
+    data = np.random.rand(i*100 + 3,i*100 + 3)
+    start = t.time()
+    pcc = PCA(2)
+    new = pcc.fit_transform(data)
+    end = t.time()
+    result_sklearn.append(end-start)
+    
+    start = t.time()
+    xx = pca(2,data)
+    end = t.time()
+    result_our.append(end-start)
+    
+x = np.linspace(0,s,num= s)
+plt.plot(x , result_our , c = 'b' , label = 'Our PCA')
+plt.plot(x , result_sklearn , c = 'r', label = 'Sklearn PCA')
+plt.plot(x_cube(x) , c = 'g' , label = 'Cube Function')
+plt.legend()
+plt.xlabel('Size Of Matrix ')
+plt.ylabel('Time in Second (S)')
+plt.title('Time Taken By Algorithms')
 plt.show()
 
-dd = np.random.rand(40*100,40*100)
-start = t.time()
-xx = pcav(2,dd)
-end = t.time()
-end-start
-from sklearn.decomposition import PCA
-start = t.time()
-pcc = PCA(2)
-new = pcc.fit_transform(dd)
-end = t.time()
-
-end-start
