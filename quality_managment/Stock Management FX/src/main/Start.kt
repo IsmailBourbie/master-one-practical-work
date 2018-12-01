3b46d55e06ef400a96834d721633b048ecbd6602
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-01 20:15:46 +0100

    QM : Fixing Problem of FXML

--- quality_managment/Stock Management FX/src/main/Start.kt
+++ quality_managment/Stock Management FX/src/main/Start.kt
@@ -9,9 +9,11 @@ import javafx.stage.Stage
 class Start : Application() {
 
     override fun start(primaryStage: Stage) {
-        val root = FXMLLoader.load<Parent>(javaClass.getResource("login.fxml"))
+
+        val root = FXMLLoader.load<Parent>(javaClass.classLoader.getResource("ressources/fxmls/login.fxml"))
         primaryStage.title = "Stock Management"
         primaryStage.scene = Scene(root, 600.0, 400.0)
+        primaryStage.isResizable = false;
         primaryStage.show()
     }
 }
