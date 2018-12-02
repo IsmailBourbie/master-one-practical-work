9ceec37c9accce65c5175ef54882b8f1d0e5d41e
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-02 11:57:43 +0100

    Fixing The Login Contollers

--- quality_managment/Stock Management FX/src/controllers/LoginController.kt
+++ quality_managment/Stock Management FX/src/controllers/LoginController.kt
@@ -2,11 +2,15 @@ package controllers
 
 import database.Database
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
+import javafx.scene.Parent
+import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.PasswordField
 import javafx.scene.control.TextField
+import javafx.stage.Stage
 import java.net.URL
 import java.util.*
 
@@ -25,12 +29,20 @@ class LoginController : Initializable {
         errorLabel.isVisible = false
 
         if (isLoginValid(username, password)) {
+            Database.user = username
             goToMainStage()
         }
     }
 
     private fun goToMainStage() {
-
+        val stage = errorLabel.scene.window as Stage
+        val new = Stage()
+        val root = FXMLLoader.load<Parent>(javaClass.classLoader.getResource("ressources/fxmls/main.fxml"))
+        new.title = "Main Screen"
+        new.scene = Scene(root, 600.0, 400.0)
+        new.isResizable = false
+        stage.close()
+        new.show()
     }
 
     private fun isLoginValid(username: String, password: String): Boolean {
