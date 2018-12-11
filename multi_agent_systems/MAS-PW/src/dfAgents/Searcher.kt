5417e76ca58cfa33ae3a56640f3b9ed4d9f68e80
Younes Charfaoui <mxcsyounes@gmail.com>
2018-12-11 10:17:58 +0100

    MAS: Refactoring Some of The Service and Searcher Code

--- multi_agent_systems/MAS-PW/src/dfAgents/Searcher.kt
+++ multi_agent_systems/MAS-PW/src/dfAgents/Searcher.kt
@@ -7,70 +7,71 @@ import jade.domain.DFService
 import jade.domain.FIPAAgentManagement.DFAgentDescription
 import jade.domain.FIPAAgentManagement.ServiceDescription
 import jade.domain.FIPAException
-
-import javax.swing.*
-import java.awt.*
+import java.awt.FlowLayout
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JTextField
 
 class Searcher : Agent() {
 
-    lateinit var Etiq: JLabel
-    lateinit var Ts: JTextField
+    lateinit var label: JLabel
+    lateinit var textField: JTextField
     internal var flag = false
-    private lateinit var fen: MaFenetre
-    // The list of agents that propose the service
-    private lateinit var ServiceAgents: MutableList<AID>
+    private lateinit var window: SearcherWindow
+    private lateinit var serviceAgents: MutableList<AID>
 
-    internal inner class MaFenetre : JFrame(), ActionListener {
+    internal inner class SearcherWindow : JFrame(), ActionListener {
 
-        private val boutonFin: JButton
-        private val boutonCalcul: JButton
+        private val endButton: JButton
+        private val calculateButton: JButton
 
         init {
             title = "Agent Client: $localName"
             setSize(250, 100)
-            val contenu = contentPane
-            contenu.layout = FlowLayout()
-            Etiq = JLabel("Type of service.")
-            contenu.add(Etiq)
-            Ts = JTextField(15)
-            contenu.add(Ts)
-            boutonCalcul = JButton("Search")
-            contenu.add(boutonCalcul)
-            boutonCalcul.addActionListener(this)
-            boutonFin = JButton("Kill")
-            contenu.add(boutonFin)
-            boutonFin.addActionListener(this)
+            val container = contentPane
+            container.layout = FlowLayout()
+            label = JLabel("Type of service.")
+            container.add(label)
+            textField = JTextField(15)
+            container.add(textField)
+            calculateButton = JButton("Search")
+            container.add(calculateButton)
+            calculateButton.addActionListener(this)
+            endButton = JButton("Kill")
+            container.add(endButton)
+            endButton.addActionListener(this)
         }
 
         override fun actionPerformed(e: ActionEvent) {
-            if (e.source === boutonCalcul)
+            if (e.source === calculateButton)
                 flag = true
             else
                 doDelete()
         }
-    } // fin de la classe MaFenetre
+    }
 
     override fun setup() {
-        fen = MaFenetre()
-        fen.isVisible = true
+        window = SearcherWindow()
+        window.isVisible = true
         while (!flag) {
         }
-        // Ajouter un TickerBehaviour pour chercher les agents proposant le service demandé toutes les 10 seconds
+
         addBehaviour(object : TickerBehaviour(this, 10000) {
             override fun onTick() {
-                // Mise à jour de la liste des agents service
+
                 val template = DFAgentDescription()
                 val sd = ServiceDescription()
-                sd.type = Ts.text
+                sd.type = textField.text
                 template.addServices(sd)
                 try {
                     val result = DFService.search(myAgent, template)
-                    ServiceAgents = mutableListOf<AID>()
-                    println("----------------------- Les agents proposant ce service sont------------------- -------")
+                    serviceAgents = mutableListOf()
+                    println("----------------------- The agents proposing this service Are --------------------------")
                     for (i in result.indices) {
-                        ServiceAgents.add(result[i].name)
+                        serviceAgents.add(result[i].name)
                         println(result[i].name)
                     }
                     if (result.isEmpty())
@@ -78,15 +79,12 @@ class Searcher : Agent() {
                 } catch (fe: FIPAException) {
                     fe.printStackTrace()
                 }
-
-            } // fin de la method onTick
+            }
         })
-    }// fin de setup
+    }
 
     override fun takeDown() {
-        // Close the GUI
-        fen.dispose()
-        // Printout a dismissal message
+        window.dispose()
         println("Service-agent: " + aid.name + " terminating.")
-    } // fin de takeDown
-}// fin de la classe searcher
+    }
+}
--- multi_agent_systems/MAS-PW/src/dfAgents/ServiceAgent.kt
+++ multi_agent_systems/MAS-PW/src/dfAgents/ServiceAgent.kt
@@ -1,70 +1,73 @@
 package dfAgents
 
-import jade.core.*
-import java.awt.*
-import java.awt.event.*
-import javax.swing.*
+import jade.core.Agent
 import jade.domain.DFService
-import jade.domain.FIPAException
 import jade.domain.FIPAAgentManagement.DFAgentDescription
 import jade.domain.FIPAAgentManagement.ServiceDescription
+import jade.domain.FIPAException
+import java.awt.FlowLayout
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JTextField
 
 class ServiceAgent : Agent() {
 
-
-    lateinit var Etiq: JLabel
-    lateinit var Etiq2: JLabel
-    lateinit var Ts: JTextField
-    lateinit var Ns: JTextField
+    lateinit var labelOne: JLabel
+    lateinit var labelTwo: JLabel
+    lateinit var textFieldOne: JTextField
+    lateinit var textFieldTwo: JTextField
     var flag = false
-    internal lateinit var fen: MaFenetre
+    private lateinit var serviceWindow: ServiceWindow
 
-    internal inner class MaFenetre : JFrame(), ActionListener {
+    internal inner class ServiceWindow : JFrame(), ActionListener {
 
-        private val boutonFin: JButton
-        private val boutonCalcul: JButton
+        private val endButton: JButton
+        private val calculateButton: JButton
 
         init {
-            title = "Agent Prestataire: $localName"
+            title = "Agent Provider: $localName"
             setSize(250, 100)
-            val contenu = contentPane
-            contenu.layout = FlowLayout()
+            val container = contentPane
+            container.layout = FlowLayout()
 
-            Etiq = JLabel("type de service")
-            contenu.add(Etiq)
-            Ts = JTextField(15)
-            contenu.add(Ts)
-            Etiq2 = JLabel("Nom de service")
-            contenu.add(Etiq2)
-            Ns = JTextField(15)
-            contenu.add(Ns)
-            boutonCalcul = JButton("Register")
-            contenu.add(boutonCalcul)
-            boutonCalcul.addActionListener(this)
-            boutonFin = JButton("Deregister")
-            contenu.add(boutonFin)
-            boutonFin.addActionListener(this)
+            labelOne = JLabel("Type of service.")
+            container.add(labelOne)
+            textFieldOne = JTextField(15)
+            container.add(textFieldOne)
+            labelTwo = JLabel("Name of service.")
+            container.add(labelTwo)
+            textFieldTwo = JTextField(15)
+            container.add(textFieldTwo)
+            calculateButton = JButton("Register")
+            container.add(calculateButton)
+            calculateButton.addActionListener(this)
+            endButton = JButton("Deregister")
+            container.add(endButton)
+            endButton.addActionListener(this)
         }
 
         override fun actionPerformed(e: ActionEvent) {
-            if (e.source === boutonCalcul)
+            if (e.source === calculateButton)
                 flag = true
             else
                 doDelete()
         }
-    } // fin de la classe MaFenetre
+    }
 
     override fun setup() {
-        fen = MaFenetre()
-        fen.isVisible = true
+        serviceWindow = ServiceWindow()
+        serviceWindow.isVisible = true
         val dfd = DFAgentDescription()
         dfd.name = aid
         val sd = ServiceDescription()
-        //attendre l’appui sur le bouton
+
         while (!flag) {
         }
-        sd.type = Ts.text
-        sd.name = Ns.text
+        sd.type = textFieldOne.text
+        sd.name = textFieldTwo.text
         dfd.addServices(sd)
         try {
             DFService.register(this, dfd)
@@ -72,20 +75,16 @@ class ServiceAgent : Agent() {
             fe.printStackTrace()
         }
 
-        println(" Enregistrement réalisé avec succès")
-    } // fin de setup
+        println("Registering Successfully !")
+    }
 
     override fun takeDown() {
-        // Deregister from the yellow pages
         try {
             DFService.deregister(this)
         } catch (fe: FIPAException) {
             fe.printStackTrace()
         }
-
-        // Close the GUI
-        fen.dispose()
-        // Printout a dismissal message
+        serviceWindow.dispose()
         println("Service-agent: " + aid.name + "  terminating.")
-    } // fin de takeDown
-}// fin de Service
+    }
+}
