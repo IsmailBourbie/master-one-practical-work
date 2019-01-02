1e4b25abaaf1cd7df01e0888c6039ef19bb427c2
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-02 10:28:46 +0100

    Making Simple UI for subscription of client

--- QM/Client IM/app/src/main/java/com/databrains/bi4ss/activities/MainActivity.kt
+++ QM/Client IM/app/src/main/java/com/databrains/bi4ss/activities/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.databrains.bi4ss.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.databrains.bi4ss.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        subscribe_button.setOnClickListener {
+            startActivity(Intent(this, SubscribeActivity::class.java))
+        }
     }
 }
---
+++ QM/Client IM/app/src/main/java/com/databrains/bi4ss/activities/SubscribeActivity.kt
@@ -0,0 +1,13 @@
+package com.databrains.bi4ss.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.databrains.bi4ss.R
+
+class SubscribeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subscribe)
+    }
+}
