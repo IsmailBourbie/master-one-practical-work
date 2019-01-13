f703576ab2c23ab8ff08b42e9b63686168637b07
Younes Charfaoui <mxcsyounes@gmail.com>
2019-01-13 21:12:41 +0100

    Completing ANdroid App, need test

--- QM/Client IM/app/src/main/java/com/databrains/bi4ss/activities/SubscribeActivity.kt
+++ QM/Client IM/app/src/main/java/com/databrains/bi4ss/activities/SubscribeActivity.kt
@@ -2,12 +2,23 @@ package com.databrains.bi4ss.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.databrains.bi4ss.IMWebService
 import com.databrains.bi4ss.R
+import com.databrains.bi4ss.models.Response
 import kotlinx.android.synthetic.main.activity_subscribe.*
+import retrofit2.Call
+import retrofit2.Callback
 
-class SubscribeActivity : AppCompatActivity() {
+class SubscribeActivity : AppCompatActivity(), Callback<Response> {
+    override fun onFailure(call: Call<Response>, t: Throwable) {
+        Log.e("Client-Error" ,  t.message)
+    }
+
+    override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+        Log.d("Client" , response.body().toString())
+    }
 
     private val webService: IMWebService = IMWebService.retrofit
             .create(IMWebService::class.java)
@@ -19,20 +30,25 @@ class SubscribeActivity : AppCompatActivity() {
 
         done_subscribe_button.setOnClickListener {
             if (validateTexts()) {
-                val society = society_et.text.trim()
-                val civility = civility_et.text.trim()
-                val fName = fname_et.text.trim()
-                val lName = lname_et.text.trim()
-                val address = address_et.text.trim()
-                val postal = postalcode_et.text.trim()
-                val country = country_et.text.trim()
-                val phone = phone_et.text.trim()
-                val mobile = mobile_et.text.trim()
-                val fax = fax_et.text.trim()
-                val email = mail_et.text.trim()
-                val type = type_et.text.trim()
+                val society = society_et.text.toString().trim()
+                val civility = civility_et.text.toString().trim()
+                val fName = fname_et.text.toString().trim()
+                val lName = lname_et.text.toString().trim()
+                val address = address_et.text.toString().trim()
+                val postal = postalcode_et.text.toString().trim()
+                val country = country_et.text.toString().trim()
+                val city = city_ed.text.toString().trim()
+                val phone = phone_et.text.toString().trim()
+                val mobile = mobile_et.text.toString().trim()
+                val observation = observation_et.text.toString().trim()
+                val fax = fax_et.text.toString().trim()
+                val email = mail_et.text.toString().trim()
+                val type = type_et.text.toString().trim()
 
-                //webService.registerClient()
+                webService.registerClient(society, civility, fName, lName, address,
+                        postal, city, country, phone, mobile,
+                        fax, email, type, livreSameAddress_radio.isChecked, exemptTva_radio.isChecked,
+                        factureSameAddress_radio.isChecked , observation).enqueue(this)
 
             } else Toast.makeText(this, "You missed some field", Toast.LENGTH_LONG).show()
         }
